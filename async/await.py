109c3b6c7a33aea4eebb4c2f71ab85b2f87101b9
alirezaatabaki <alirezaatabaki@icloud.com>
2020-10-23 19:21:51 +0330

    async and await using run method

---
+++ async/await.py
@@ -0,0 +1,10 @@
+"""
+    coroutine is a function that can suspend its execution before reaching return.
+"""
+import asyncio
+
+async def show():
+    await asyncio.sleep(3)
+    print("HELLO WORLD...")
+
+asyncio.run(show())
